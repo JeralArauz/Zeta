2747de692a2a4672eeedfdf26f5f1ec504658d17
Agenor! <gerald08.kk@gmail.com>
2022-10-13 11:47:09 -0600

    Formulario Denominaciones.

--- Zetabyte/Catálogos/Denominaciones.aspx
+++ Zetabyte/Catálogos/Denominaciones.aspx
@@ -5,7 +5,19 @@
     <div class="row card-header justify-content-center font-weight-bold main" style="background-color:#00283C; color:white">DENOMINACIONES</div>
     <div class="row">
         <div class="col-md-12" style="padding-left: 30px; padding-right: 30px;">
-
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                    <asp:Label ID="Lbl" runat="server" Text=" Mensaje :" Visible="false" Font-Names="Tahoma" Font-Size="Small" ForeColor="#006600" Width="100%" Height="20px" Font-Bold="True">
+                    </asp:Label>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+            <asp:UpdatePanel ID="UpdatePanel2" runat="server" UpdateMode="Conditional">
+                <ContentTemplate>
+                    <asp:Label ID="Lbl_Mensaje" runat="server" Text=" "
+                        Font-Names="Tahoma" Font-Size="Small" ForeColor="Blue" Width="430px"></asp:Label>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+            <dx:ASPxGridView ID="GridViewDenominaciones" runat="server"></dx:ASPxGridView>
         </div>
     </div>
 </asp:Content>
--- Zetabyte/Catálogos/Denominaciones.aspx.designer.cs
+++ Zetabyte/Catálogos/Denominaciones.aspx.designer.cs
@@ -13,5 +13,50 @@ namespace Zetabyte.Catálogos
 
     public partial class Denominaciones
     {
+
+        /// <summary>
+        /// Control UpdatePanel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+
+        /// <summary>
+        /// Control Lbl.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Lbl;
+
+        /// <summary>
+        /// Control UpdatePanel2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel2;
+
+        /// <summary>
+        /// Control Lbl_Mensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Lbl_Mensaje;
+
+        /// <summary>
+        /// Control GridViewDenominaciones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::DevExpress.Web.ASPxGridView GridViewDenominaciones;
     }
 }
